027b5295803685065ce5f1873ff214e5ee6f7a5d
VugarJabrailzade <vugarej@code.edu.az>
2024-03-02 12:28:57 +0400

    Fixing Ordepage Navtabs border

--- assets/Js/order.js
+++ assets/Js/order.js
@@ -161,3 +161,26 @@ tabLink.forEach((tag)=>{
 //         console.log("Bi siqeet")
 //     }
 // })
+
+var tabList = document.querySelectorAll(".nav-tabs-head-list");
+
+tabList.forEach(function(tab) {
+    tab.addEventListener("click", (e) => {
+        e.preventDefault();
+        var clickedTab = e.target.parentElement;
+
+        if (clickedTab.tagName === "UL") {
+            return;
+        }
+
+        tabList.forEach(function(tab) {
+            if (tab !== clickedTab && tab.classList.contains("nav-tabs-active")) {
+                tab.classList.remove("nav-tabs-active");
+            }
+        });
+
+        clickedTab.classList.add("nav-tabs-active");
+        console.log(clickedTab);
+    });
+});
+
